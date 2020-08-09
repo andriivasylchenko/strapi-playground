8ac65b837828a230b2e2afe2fb787566f3aa3e88
Andrii Vasylchenko <andrii@ibm.com>
2020-08-09 12:14:08 -0400

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4d6b1fa817e881cd289d62a167b54a47'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4fb86875-3cb3-491b-8860-e96aba63f904'
+};
